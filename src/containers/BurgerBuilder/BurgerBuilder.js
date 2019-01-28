8cf141417c5f823ca8d80f0e4967fd85d4f65d82
Kamratow <kamilratowski90@gmail.com>
2019-01-28 14:12:03 +0100

    Added post call in order summary and fixed bug with side drawer

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -84,7 +85,24 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-	  alert('You continue!');
+		// alert('You continue!');
+		const order = {
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: {
+				name: 'John Doe',
+				address: {
+					street: 'TestingStreet 4',
+					zipCode: '44789',
+					country: 'Poland'
+				},
+				email: 'test@test.com'
+			},
+			deliveryMethod: 'fast'
+		}
+		axios.post('/orders.json', order)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
   }
 
   render () {
