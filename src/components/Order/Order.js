f5e58f235fa836b8305c932649585a1fb1bf5d27
Kamratow <kamilratowski90@gmail.com>
2019-02-05 16:18:48 +0100

    Added Orders page to the app

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>ingr</p>
+        <p>price<strong>2 dollars</strong></p>
+    </div>
+);
+
+export default order;
