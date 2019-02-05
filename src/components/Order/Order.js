f452ffe0ee7c7b62651e9dcacdf81c8e265016aa
Kamratow <kamilratowski90@gmail.com>
2019-02-05 17:06:53 +0100

    Added outputing orders in the Orders component

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,36 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>ingr</p>
-        <p>price<strong>2 dollars</strong></p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName,
+                ammount: props.ingredients[ingredientName]
+            }
+        );
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ig.name}>{ig.name} ({ig.ammount})</span>;
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>price<strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default order;
