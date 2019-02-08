38f7a27144cb96c7c3ccaf8fe09ea8cde10c4108
Kamratow <kamilratowski90@gmail.com>
2019-02-08 11:39:42 +0100

    Created and implemented action creators for the burger builder

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducers/bugerBuilder';
+import burgerBuilderReducer from './store/reducers/bugerBuilder';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
