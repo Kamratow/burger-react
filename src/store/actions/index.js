d876e1e4c968b39851e72fd171f65624ff110eb7
Kamratow <kamilratowski90@gmail.com>
2019-03-26 21:03:50 +0100

    Added handling authentication in sagas

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,21 @@
-export { addIngredient, removeIngredient, initIngredients } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
-export { auth, logout, logoutSucceed, setAuthRedirectPath, authCheckState } from './auth';
+export { 
+    addIngredient, 
+    removeIngredient, 
+    initIngredients 
+} from './burgerBuilder';
+export { 
+    purchaseBurger, 
+    purchaseInit, 
+    fetchOrders 
+} from './order';
+export { 
+    auth,
+    authStart, 
+    logout, 
+    logoutSucceed, 
+    setAuthRedirectPath, 
+    authCheckState,
+    authSuccess,
+    authFail,
+    checkAuthTimeout
+} from './auth';
